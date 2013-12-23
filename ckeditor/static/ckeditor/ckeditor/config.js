962ed16642a9e8d39bb75b10e8b1139ffd5ed454
Enric Caumons <caumons@gmail.com>
2013-12-23 14:50:42 +0100

    [CHN] Added title attribute for img tag

--- ckeditor/static/ckeditor/ckeditor/config.js
+++ ckeditor/static/ckeditor/ckeditor/config.js
@@ -43,7 +43,7 @@ CKEDITOR.editorConfig = function( config ) {
 
     // Code not produced by CKEditor will be stripped to prevent malicious attacks
     config.allowedContent = false;
-    config.extraAllowedContent = 'iframe[*]; div[*]; img[!src, alt, width, height]; *(*){*};';
+    config.extraAllowedContent = 'iframe[*]; div[*]; img[!src, alt, width, height, title]; *(*){*};';
 
     // Don't use HTML entities
     config.entities = false;
